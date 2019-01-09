75101b17f72bd2358bd72135447d1436721b36d0
Gichia <petergichia35@gmail.com>
2019-01-09 17:04:07 +0300

    Added one test

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from .api.v1.views.user_views import ver1 as v1
 
 
-def create_app(config):
+def create_app():
     '''function creating the flask app'''
     app = Flask(__name__)
     app.register_blueprint(v1)
---
+++ app/tests/test_meetup.py
@@ -0,0 +1,31 @@
+import json
+import unittest
+from app import create_app
+
+APP = create_app()
+
+class TestMeetup(unittest.TestCase):
+
+    def setUp(self):
+        APP.testing = True
+        self.app = APP.test_client()
+        self.data = {
+            "id": 1,
+            "created_on": "12/12/1018",
+            "location": "Nairobi",
+            "topic": "Web Dev",
+            "tags": ["Flask", "API"]
+        }
+
+    def test_create_meetups(self):
+        
+        response = self.app.post('/api/v1/meetup', data = json.dumps(self.data), content_type="application/json")
+        result = json.loads(response.data.decode('UTF-8'))
+        result.asserEqual(result["data"]["id"], 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
